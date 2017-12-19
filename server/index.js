4f86e107d7f04ceafdaa59d8f7fbb5b2a07e404c
meganmfisher <meganmichellefisher@gmail.com>
2017-12-19 10:01:43 -0700

    backend fully setup

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const session = require('express-session');
 const massive = require('massive');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 const user_controller = require('./controllers/user_controller')
 const property_controller = require('./controllers/property_controller')
 require('dotenv').config();
@@ -12,7 +11,7 @@ const app = express();
 
 massive( process.env.CONNECTIONSTRING ).then( dbInstance => {
   app.set('db', dbInstance);
-  //Sets up tables in DB.
+  //Sets up tables in DB for you.
   app.get('db').initialize_db().then(response => {
       console.log(response)
   })
@@ -22,28 +21,27 @@ massive( process.env.CONNECTIONSTRING ).then( dbInstance => {
 //MIDDLEWARE
 app.use( bodyParser.json() );
 app.use( cors() );
+
 app.use( session({
   secret: '@ny7h1ng y0u w@n7',
   resave: false,
   saveUninitialized: false
 }));
+
 app.use( require(`./middlewares/auth`) );
 app.use( express.static(`${__dirname}/../build`) );
 
 
-//ENDPOINTS
+// LOGIN ENDPOINTS
 app.post('/api/auth/login', user_controller.login);
 app.post('/api/auth/register', user_controller.register);
 app.post('/api/auth/logout', user_controller.logout);
 
-
+// PROPERTY LISTINGS ENDPOINTS
 app.post('/api/properties/', property_controller.create);
 app.get('/api/properties/', property_controller.readAll);
 app.delete('/api/properties/:id', property_controller.delete);
 app.get('/api/properties/filter', property_controller.filter);
 
-// app.get('*', ( req, res, next ) => {
-//   res.sendFile( path.resolve('../build/index.html') );
-// });
 
 app.listen( process.env.PORT, () => { console.log(`Server listening on port ${ process.env.PORT}`); } );
